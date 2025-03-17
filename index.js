e26f366e638675a4da9ae2ca8815c90c07b817a4
Arturocs160 <kronculd@outlook.com>
2025-03-17 13:04:11 -0600

    Se añade cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const routes = require('./src/routes')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
 
 const PORT = 3001;
 
